3d5b3414c8fcf98a11d46815a1f588dacc57f72b
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-21 08:46:20 +0100

    support multiple urls to cycle through

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 var opn = require('opn');
 
+var index = 0;
+var youtubeUrls = [
+  "https://youtu.be/5-xhpcgBMe4?t=84",
+  "https://youtu.be/5-xhpcgBMe4?t=14",
+  "https://youtu.be/5-xhpcgBMe4?t=184",
+]
+
 /* GET home page. */
 router.post('/', function(req, res, next) {
   console.log('body', req.body);
-  opn("https://youtu.be/5-xhpcgBMe4?t=84");
+  var indexToOpen = index++;
+  index = index % youtubeUrls.length;
+  opn(youtubeUrls[indexToOpen]);
   res.status(200);
   res.send();
 });
