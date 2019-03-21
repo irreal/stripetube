31e79881e90fff2d1478bcd51f794b3931556a6e
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-21 12:48:47 +0100

    updated playlist

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,16 @@ var opn = require('opn');
 
 var index = 0;
 var youtubeUrls = [
-  "https://youtu.be/5-xhpcgBMe4?t=84",
-  "https://youtu.be/5-xhpcgBMe4?t=14",
-  "https://youtu.be/5-xhpcgBMe4?t=184",
+"https://youtu.be/_JZom_gVfuw?t=18", 
+"https://youtu.be/2ksuJcnAqR8?t=20",
+"https://youtu.be/_3tCkBKAEr4?t=27",
+"https://youtu.be/8NHnD7vappI?t=24",
+"https://youtu.be/_lb0HImG_MU?t=16",
+"https://youtu.be/PBwAxmrE194?t=86",
+"https://youtu.be/Fi4S3WXLgso?t=70",
+"https://youtu.be/smqhSl0u_sI?t=79",
+"https://youtu.be/bWXazVhlyxQ?t=35",
+"https://youtu.be/McJcDToEiyw?t=32",
 ]
 
 /* GET home page. */
